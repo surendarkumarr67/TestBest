48776a2b111f392d9aa7612953935cdfe2174513
surendra <surendarkumarr67@gmail.com>
2022-12-01 12:21:47 +0530

    Test the best

---
+++ UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+
+namespace TestRestSharp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            /**
+             * 1. Create the client
+             * 2. Create the request
+             * 3. Send the request
+             * 4. Capture the response
+             * 5. Add the verification on the response.
+             * **/
+
+            RestClient client = new RestClient();
+            RestRequest request = new RestRequest();
+
+        }
+    }
+}
